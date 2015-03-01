0c8ae893837a593868b7961c2659abe6cf9cfa6e
Bertrand Dunogier <bd@ez.no>
2015-03-01 21:55:48 +0100

    Implemented Tvsubtitles parser

--- vendor/bdunogier/subber/lib/Release/Parser/SubtitleRelease/TvsubtitlesParser.php
+++ vendor/bdunogier/subber/lib/Release/Parser/SubtitleRelease/TvsubtitlesParser.php
@@ -6,13 +6,11 @@ use BD\Subber\Release\Parser\ReleaseParserException;
 use BD\Subber\Subtitles\Subtitle;
 
 /**
- * Parses subtitles names from Addic7ed
+ * Parses subtitle releases from tvsubtitles.net
  *
- * Vikings - 03x02 - The Wanderer.WEB-DL-BS.English.C.orig.Addic7ed.com.srt
- * Bitten - 02x04 - Dead Meat.KILLERS.English.C.orig.Addic7ed.com.srt
- * Allegiance - 01x04 - Chasing Ghosts.LOL.French.C.updated.Addic7ed.com
+ *
  */
-class Addic7edParser implements ReleaseParser
+class TvsubtitlesParser implements ReleaseParser
 {
     /**
      * @param string $releaseName
@@ -20,80 +18,20 @@ class Addic7edParser implements ReleaseParser
      */
     public function parseReleaseName( $releaseName )
     {
-        $release = new Subtitle( ['name' => $releaseName] );
-        $release->author = 'addic7ed';
-
-        // addic7ed.com
+        $release = new Subtitle( ['name' => $releaseName, 'author' => 'tvsubtitles'] );
         $releaseParts = explode( '.', strtolower( $releaseName ) );
-        if ( array_pop( $releaseParts ) != 'com' || array_pop( $releaseParts ) != 'addic7ed' )
-            throw new ReleaseParserException( $releaseName, "addic7ed.com string not found" );
 
-        // orig or updated
-        $status = array_pop( $releaseParts );
-        if ( !in_array( $status, ['orig', 'updated'] ) ) {
-            throw new ReleaseParserException( $releaseName, "$status isn't a valid status" );
-        }
+        $release->subtitleFormat = array_pop( $releaseParts );
+        $release->language = array_pop( $releaseParts );
 
-        // C thing in the release name
-        $c = array_pop( $releaseParts );
-        if ( $c != 'c' )
-            throw new ReleaseParserException( $releaseName, "$c isn't the 'C' thing" );
-
-        // can be hearing impaired, or language
-        $next = array_pop( $releaseParts );
-        if ( $next == 'hi' )
-        {
-            $release->isHearingImpaired = true;
+        $release->group = array_pop( $releaseParts );
+        if ( ( $next = array_pop( $releaseParts ) ) === 'lol+720p' ) {
+            $release->group = [$release->group, 'lol'];
             $next = array_pop( $releaseParts );
         }
 
-        // language
-        if ( in_array( $next, ['english', 'french'] ) ) {
-            $release->language = $this->getLanguageCode( $next );
-        }
-
-        // group thing
-        list( $release->source, $release->group ) = $this->resolveSourceThing( array_pop( $releaseParts ) );
+        $release->source = $next;
 
         return $release;
-    }
-
-    private function resolveSourceThing( $sourceThingString )
-    {
-        $source = null;
-        $group = null;
-
-        if ( $sourceThingString === 'web-dl' )
-        {
-            $source = 'web-dl';
-        }
-
-        // killers-translate, web-dl-bs, ...
-        if ( preg_match( '/^(.*?)\-([^\-]+)$/', $sourceThingString, $m ) )
-        {
-            if ( $m[1] == 'web-dl' ) {
-                $source = $m[1];
-                $group = $m[2];
-            } else {
-                $group = $m[1];
-                if ( $m[2] != 'translate' ) {
-                    $source = $m[2];
-                }
-            }
-
-        }
-
-        if ( $group === null )
-        {
-            $group = $sourceThingString;
-        }
-
-        return [$source, $group];
-    }
-
-    private function getLanguageCode( $languageString )
-    {
-        $map = ['english' => 'en', 'french' => 'fr'];
-        return isset( $map[$languageString] ) ? $map[$languageString] : null;
     }
 }
---
+++ vendor/bdunogier/subber/tests/lib/Release/Parser/SubtitleRelease/TvsubtitlesParserTest.php
@@ -0,0 +1,58 @@
+<?php
+namespace tests\BD\Subber\Release\Parser\SubtitleRelease;
+
+use BD\Subber\Release\Parser\SubtitleRelease\Addic7edParser;
+use BD\Subber\Release\Parser\SubtitleRelease\TvsubtitlesParser;
+
+class TvsubtitlesParserTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \BD\Subber\Release\Parser\SubtitleRelease\Addic7edParser */
+    private $parser;
+
+    /**
+     * @dataProvider getValidReleases
+     */
+    public function testParseValidRelease( $releaseName, $expectedProperties )
+    {
+        $parser = new TvsubtitlesParser();
+        $release = $parser->parseReleaseName( $releaseName );
+
+        foreach ( ['author' => 'tvsubtitles'] + $expectedProperties as $property => $value )
+        {
+            self::assertAttributeEquals( $value, $property, $release );
+        }
+    }
+
+    public function getValidReleases()
+    {
+        return [
+            [
+                'Gotham - 1x11 - Episode 11.HDTV.LOL.fr.srt',
+                [
+                    'name' => 'Gotham - 1x11 - Episode 11.HDTV.LOL.fr.srt',
+                    'group' => 'lol',
+                    'language' => 'fr',
+                    'source' => 'hdtv'
+                ]
+            ],
+            [
+                'Gotham - 1x16 - The Blind Fortune Teller.HDTV.LOL+720p.DIMENSION.fr.srt',
+                [
+                    'name' => 'Gotham - 1x16 - The Blind Fortune Teller.HDTV.LOL+720p.DIMENSION.fr.srt',
+                    'source' => 'hdtv',
+                    'group' => ['dimension', 'lol'],
+                    'language' => 'fr'
+                ]
+            ],
+            [
+                'Gotham - 1x16 - The Blind Fortune Teller.HDTV.LOL.en.srt',
+                [
+                    'name' => 'Gotham - 1x16 - The Blind Fortune Teller.HDTV.LOL.en.srt',
+                    'group' => 'lol',
+                    'language' => 'en',
+                    'source' => 'hdtv'
+                ]
+            ]
+        ];
+    }
+}
