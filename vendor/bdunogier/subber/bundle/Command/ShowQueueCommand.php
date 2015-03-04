9ac5587798e95d40dbba8d3be8d1b267ce208d46
Bertrand Dunogier <bd@ez.no>
2015-03-04 22:40:16 +0100

    Added subber:show-queue command

---
+++ vendor/bdunogier/subber/bundle/Command/ShowQueueCommand.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the eZ Publish Kernel package
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace BD\SubberBundle\Command;
+
+use BD\Subber\Entity\Task;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ShowQueueCommand extends ContainerAwareCommand
+{
+    public function configure()
+    {
+        $this->setName( 'subber:show-queue' );
+    }
+
+    public function execute( InputInterface $input, OutputInterface $output )
+    {
+        $repository = $this->getContainer()->get( 'bd_subber.tasks_repository' );
+
+        $output->writeln( "Pending tasks" );
+        $output->writeln( "" );
+
+        /** @var Task $task */
+        foreach ($repository->findAllPendingTasks() as $task) {
+            $output->writeln(
+                sprintf( "- %s (%s)", $task->getOriginalName(), $task->getFile() )
+            );
+        }
+    }
+}
