4ec8b416c2daa77513bb3ed24ef6e5c3879051ee
Bertrand Dunogier <bd@ez.no>
2015-03-08 15:31:37 +0100

    Oops

--- vendor/bdunogier/subber/bundle/Command/ShowQueuedReleaseCommand.php
+++ vendor/bdunogier/subber/bundle/Command/ShowQueuedReleaseCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ProcessQueuedReleaseCommand extends ContainerAwareCommand
+class ShowQueuedReleaseCommand extends ContainerAwareCommand
 {
     public function configure()
     {
