389963e7aa8705cbd731816dacfccde86a90f5d8
Bertrand Dunogier <bd@ez.no>
2015-05-02 22:56:08 +0200

    Fixed RestController's name

--- vendor/bdunogier/subber/bundle/Controller/RestController.php
+++ vendor/bdunogier/subber/bundle/Controller/RestController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class WatchListController extends Controller
+class RestController extends Controller
 {
     /** @var \BD\Subber\WatchList\WatchList */
     private $watchList;
